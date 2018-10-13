d23fee531672b935439e6d4d853a78aa21eace5b
ITnull <1209016337@qq.com>
2018-10-13 13:46:53 +0800

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Moment(models.Model):
+    content=models.CharField(max_length=200)
+    user_name=models.CharField(max_length=20)
+    kind=models.CharField(max_length=20)
