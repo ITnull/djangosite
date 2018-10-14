67c391f781aeba906a731f18a795364d3352a599
ITnull <1209016337@qq.com>
2018-10-14 08:54:58 +0800

    model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
+#-*-coding:utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
 
+#新增元组用于设置消息类型枚举项
+KIND_CHOICES=(
+	('python技术','python技术'),
+	('数据库技术','数据库技术'),
+	('经济学','经济学'),
+	('文体资讯','文体资讯'),
+	('个人心情','个人心情'),
+	('其他','其他'),
+)
+
+
 class Moment(models.Model):
     content=models.CharField(max_length=200)
-    user_name=models.CharField(max_length=20)
-    kind=models.CharField(max_length=20)
+    user_name=models.CharField(max_length=20,default='匿名')
+    kind=models.CharField(max_length=20,choices=KIND_CHOICES,default=KIND_CHOICES[0])
--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+import os
+from app.forms import MomentForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,4 +11,15 @@ from django.http import HttpResponse
 def welcome(request):
     return HttpResponse("<h1>welcome to my tiny twitter!</h1>")
 
+def moments_input(request):
+    if(request.method=='POST'):
+	form=MomentForm(request.POST)
+        if(form.is_valid()):
+	    moment=form.save()
+	    moment.save()
+	    return HttpResponseRedirect(reverse("app.views.welcome"))
+    else:
+	form=MomentForm()
+    PROJECT_ROOT=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return render(request,os.path.join(PROJECT_ROOT,'app/templates','moments_input.html'),{'form':form})
 
